c2aa339aa0289dda65540b3d93b6ed08b7a58a36
Mitsuki Yanagi <iganaymjs2@gmail.com>
2024-08-31 19:11:20 +0900

    add:不要なファイル作成や追記をしないようにする

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,6 @@
 Rails.application.config.generators do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    g.skip_routes true
+    g.stylesheets false # 新しいコントローラや他の生成物を作成する際に、スタイルシート（CSSファイル）が自動的に生成されないようにする
+    g.javascripts false # 新しいコントローラや他の生成物を作成する際に、JavaScriptファイルが自動的に生成されないようにする
+    g.helper false # 新しいコントローラを作成する際に、対応するヘルパーモジュール（app/helpers 内のファイル）が自動的に生成されないようにする
+    g.skip_routes true # 新しいリソースやコントローラを生成する際に、config/routes.rb に自動的にルートが追加されないようにする
 end
