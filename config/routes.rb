40db8c8849c4dcd1fee635b8b0f9a680a0cbd7f2
Mitsuki Yanagi <iganaymjs2@gmail.com>
2024-09-06 01:22:09 +0900

    add:Sessionsコントローラのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root to: 'users#show'
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
